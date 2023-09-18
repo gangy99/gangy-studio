20558fb6f8e1956760a2b5e59ce4c47eda57b53f
Angy <diseno5@Arte-y-Diseno.local>
2023-09-18 13:53:11 -0600

    Validaciones inputs

--- js/app.js
+++ js/app.js
@@ -7,3 +7,4 @@ inputs.forEach( input => {
         valida(input.target);
     });
 });
+
--- js/validaciones.js
+++ js/validaciones.js
@@ -3,14 +3,12 @@ export function valida(input) {
 
     if (input.validity.valid) {
       input.parentElement.classList.remove("input-container--invalid");
-      input.parentElement.querySelector("input-message-error").innerHTML = "";
+      input.parentElement.querySelector(".input-message-error").innerHTML = "";
     }else{
       input.parentElement.classList.add("input-container--invalid");
-      input.parentElement.querySelector("input-message-error").innerHTML = mostrarMensajeDeError (tipoDeInput, input);
+      input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
     }
-
 }
-
 
 const tipoDeErrores = [
     "valueMissing",
@@ -26,19 +24,16 @@ const tipoDeErrores = [
         valueMissing: "El campo teléfono no puede estar vacío",
         patternMismatch:
           "El formato requerido es XXXXXXXXXX (10 dígitos)",
-      },
+    },
     email: {
       valueMissing: "El campo correo no puede estar vacío",
-      typeMismatch: "El correo no es válido",
+      typeMismatch: "El correo no es válido. Ejemplo: nombre@email.com",
     },
     asunto: {
       valueMissing: "Este campo no puede estar vacío",
-      atternMismatch:
+      patternMismatch:
           "El asunto debe tener entre 2 y 40 caracteres",
     },
-    mensaje: {
-        valueMissing: "Este campo no puede estar vacío",
-      },
   };
 
   function mostrarMensajeDeError(tipoDeInput, input) {
@@ -51,5 +46,29 @@ const tipoDeErrores = [
         mensaje = mensajesDeError[tipoDeInput][error];
       }
     })
+    return mensaje;
+  }
+
+  const textarea = document.querySelector("textarea");
+
+  textarea.addEventListener('blur', (textarea) =>{
+    textarea.target;
+    console.log(textarea);
+  });
+
+  if (errorTextarea) {
+    textarea.parentElement.classList.remove("textarea-container--invalid");
+    textarea.parentElement.querySelector(".textarea-mensagem-erro").innerHTML = "";
+  }else{
+    textarea.parentElement.classList.add("textarea-container--invalid");
+    textarea.parentElement.querySelector(".textarea-mensagem-erro").innerHTML = errorTextarea(textarea);
+  }
+
+
+  function errorTextarea(textarea) {
+    let mensaje = "";
+      if(textarea = ""){
+        mensaje = "Este campo no puede estar vacío";
+      }
     return mensaje;
   }
